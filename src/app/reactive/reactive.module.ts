93a6e798873f918c85ce1edb99ea0921e74c42c8
david <davidchilatra@gmail.com>
2023-12-07 19:20:53 -0500

    se defina fron y route de cada vista

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
 
 @NgModule({
   declarations: [
-    BasicPageComponent
+    BasicPageComponent,
+    DynamicPageComponent,
+    DynamicPageComponent,
+    SwitchesPageComponent
   ],
   imports: [
     CommonModule,
