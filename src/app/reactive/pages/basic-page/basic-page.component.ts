7067b1ed1603ebaafd693065ee52024897a26318
david <davidchilatra@gmail.com>
2023-12-07 21:11:53 -0500

    se realizan validaciones básicas

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'rtx5090',
+  price: 2500,
+  inStorage: 10
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 /*
   public myform: FormGroup = new FormGroup({
     //valor defecto, validaciones sincronas, validaciones asincronas
@@ -17,16 +23,61 @@ export class BasicPageComponent {
   */
 
   public myForm: FormGroup = this.formbuilder.group({
-    name: ['',[],[]],
-    price: [0],
-    inStorage: [0],
+    name: ['',[
+      Validators.required,
+      Validators.minLength(3)
+    ],[]],
+    price: [0,[
+      Validators.required,
+      Validators.min(0)
+    ]],
+    inStorage: [0,[
+      Validators.required,
+      Validators.min(0)
+    ]],
   })
  public constructor(
   private formbuilder: FormBuilder,
  ){}
 
-  onSave():void{
-    console.log(this.myForm.value)
+  ngOnInit(): void {
+    //this.myForm.reset(rtx5090)
   }
 
+  onSave():void{
+    if(this.myForm.invalid) {
+      //Para que se activen validaciones que se muestran solo cuando se muestra el formulario
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value)
+
+    this.myForm.reset({price:10, inStorage: 0});
+  }
+
+  isValidField(field:string): boolean | null{
+    return this.myForm.controls[field].errors
+            && this.myForm.controls[field].touched
+  }
+
+  getFieldError(field:string):string | null{
+    if(!this.myForm.controls[field] &&
+       !this.myForm.controls[field].errors
+      ) return null;
+
+    //En este punto los errores siempre existen
+    const errors = this.myForm.controls[field].errors!
+
+    for (const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`
+      }
+    }
+
+    return null;
+  }
 }
