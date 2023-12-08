14a0e842c1872b181ae126c7ceec039ede61a8e4
david <davidchilatra@gmail.com>
2023-12-07 20:06:05 -0500

    se especifica input para formulario básico

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+/*
+  public myform: FormGroup = new FormGroup({
+    //valor defecto, validaciones sincronas, validaciones asincronas
+    name: new FormControl('',[],[]),
+    price: new FormControl(0),
+    inStorage: new FormControl(0)
+  })
+  */
+
+  public myForm: FormGroup = this.formbuilder.group({
+    name: ['',[],[]],
+    price: [0],
+    inStorage: [0],
+  })
+ public constructor(
+  private formbuilder: FormBuilder,
+ ){}
+
+  onSave():void{
+    console.log(this.myForm.value)
+  }
 
 }
